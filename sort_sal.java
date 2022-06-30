7756703d0082847ffe9178cb1881908ff1f75849
surajbadam <72784198+surajbadam@users.noreply.github.com>
2022-06-30 16:22:23 +0530

    Add files via upload
    sort based on age

---
+++ sort_sal.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class sort_sal {
+    public static void main(String[] args) {
+        emp e1=new emp();
+        e1.setName("suraj");
+        e1.setAge(22);
+        e1.setSal(30000);
+        emp e2=new emp();
+        e2.setName("ram");
+        e2.setAge(24);
+        e2.setSal(50000);
+        emp e3=new emp();
+        e3.setName("bheem");
+        e3.setAge(20);
+        e3.setSal(10000);
+        emp e[]=new emp[3];
+        e[0]=e1;e[1]=e2;e[2]=e3;
+        Arrays.sort(e,new Comparator<emp>() {
+            @Override
+            public int compare(emp o1, emp o2) {
+                    return o1.getAge()-o2.getAge();
+                }
+            
+        });
+        for(int i=0;i<e.length;i++)
+        {
+            System.out.println(e[i].getName()+" "+e[i].getAge()+" "+e[i].getSal());
+        }
+    }
+}
